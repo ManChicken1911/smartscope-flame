31801a8bac30f45d2c6708097981d11f3fc6ac98
Bob Maple <bobm-git@burner.com>
2017-02-05 01:20:11 -0700

    New brightness and contrast control, remove double Picture mode, add new Off mode

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-SmartScope-Flame Controller
+# SmartScope-Flame Controller
 by Bob Maple (bobm-matchbox [at] idolum.com)
 
-This script is licensed under the Creative Commons Attribution-ShareAlike (CC BY-SA)
-https://creativecommons.org/licenses/by-sa/4.0/
+This script is licensed under the Creative Commons Attribution-ShareAlike [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0/)
 
 
 ## What
@@ -19,7 +18,7 @@ via Ethernet, so your SmartScope must be on the network.
 
 To install, first place smartscope-flame.py in the Flame's python directory:
 
-  /usr/discreet/flame_2017.1/python/
+  `/usr/discreet/flame_2017.1/python/`
 
 where 'flame_2017.1' will vary depending on your specific application and version.
 Technically this script can be placed anywhere but it's convenient to keep it with
@@ -28,28 +27,31 @@ the rest of Flame's Python code. Note that you might need to be root to do this.
 Next, edit smartscope-flame.py in a text editor and modify the ssdc_hostname config
 variable near the top to tell it how to reach your SmartScope:
 
-   #### Configuration ####
-   ssdc_hostname = '192.168.1.1'  # IP or hostname
-
+```
+#### Configuration ####
+ssdc_hostname = '192.168.1.1'  # IP or hostname
+```
 It can be specified as a hostname or IP address (shown.)
 
 Finally, to connect the code into Flame, open hook.py in a text editor and comment
 out the supplied functions getCustomUIActions and customUIAction so they look like:
 
-   # def getCustomUIActions( ):
-   #     return ()
+```
+# def getCustomUIActions( ):
+#     return ()
 
-   ...
+...
 
-   # def customUIAction( info, userData ):
-   #     pass
+# def customUIAction( info, userData ):
+#     pass
+```
 
 Then add a line either after what you just commented out or to the very end of
 the file:
 
-  execfile("/usr/discreet/flame_2017.1/python/smartscope-control.py")
+  `execfile("/usr/discreet/flame_2017.1/python/smartscope-control.py")`
 
-again replacing flame_2017.1 with the proper directory name based on your setup,
+again replacing `flame_2017.1` with the proper directory name based on your setup,
 or if you decided to put the script somewhere else entirely, replace the whole
 pathname with the correct location as appropriate.
 
@@ -57,9 +59,9 @@ pathname with the correct location as appropriate.
 everything above and have to just manually integrate the code from the hooks in
 smartscope-flame.py into your existing hooks and make it work.)
 
-Restart Flame or press Shift-Control-H-P in Flame to reload the Python hooks.
+Restart Flame or press **Shift-Control-H-P** in Flame to reload the Python hooks.
 Right-click somewhere on the Flame desktop (in the Player, Reels, Freeform view,
-etc) to pull up the context menu and you should now have a SmartScope Duo entry
+etc) to pull up the context menu and you should now have a **SmartScope Duo** entry
 at the bottom.
 
 
@@ -72,15 +74,18 @@ First there two sets of config variables for setting the screen brightness and
 constrast used for Scope displays (Parade, Vector, Waveform, etc) and the Picture
 display:
 
-   ssdc_scope    = { "brightness": "127", "contrast": "127" }
-   ssdc_picture  = { "brightness": "127", "contrast": "127" }
-
+```
+ssdc_scope    = { "brightness": "127", "contrast": "127" }
+ssdc_picture  = { "brightness": "127", "contrast": "127" }
+```
 Values are 0-255 with 127 being 'default' (50% Brightness and 100% Contrast in
 the SmartView app.)
 
 You can also swap the displays to be opposite of how they are listed in the menu:
 
-   ssdc_swap     = False
+```
+ssdc_swap     = False
+```
 
 Setting this to **True** will swap the left and right displays, so that choosing
 "Parade RGB | Vectorscope" will actually display the Vectorscope on the left monitor
--- smartscope-control.py
+++ smartscope-control.py
@@ -2,7 +2,7 @@
 # ******************************************************************************
 #
 # SmartScope Duo Controller for Flame v1.0
-# Copyright (c) 2015 Bob Maple (bobm-matchbox [at] idolum.com)
+# Copyright (c) 2015-2017 Bob Maple (bobm-matchbox [at] idolum.com)
 #
 # Adds a context menu to switch the monitors of the Blackmagic Design
 # SmartScope Duo 4K
@@ -10,6 +10,15 @@
 
 #### Configuration ####
 ssdc_hostname = '192.168.1.1'  # IP or hostname
+
+# Separate Brightness and Contrast values for Scope displays vs Picture
+# Ranges 0-255 with 127 being the 'middle' or default value
+ssdc_scope    = { "brightness": "127", "contrast": "127" }
+ssdc_picture  = { "brightness": "127", "contrast": "127" }
+
+# Swap the left and right monitors
+ssdc_swap     = False
+
 ####
 
 
@@ -26,36 +35,59 @@ def getCustomUIActions( ):
   ssdc1 = {}
   ssdc1[ "name" ] = "ssdcfunc"
   ssdc1[ "caption" ] = "Parade RGB | Vectorscope"
-  ssdc1[ "config" ] = { "mon_a": "ParadeRGB", "mon_b": "Vector75" }
+  ssdc1[ "config" ] = { \
+  "mon_a": "ParadeRGB", "mon_a_br": ssdc_scope['brightness'], "mon_a_cn": ssdc_scope['contrast'], \
+  "mon_b": "Vector75",  "mon_b_br": ssdc_scope['brightness'], "mon_b_cn": ssdc_scope['contrast']  \
+  }
 
   ssdc2 = {}
   ssdc2[ "name" ] = "ssdcfunc"
   ssdc2[ "caption" ] = "Parade RGB | Picture"
-  ssdc2[ "config" ] = { "mon_a": "ParadeRGB", "mon_b": "Picture" }
+  ssdc2[ "config" ] = { \
+  "mon_a": "ParadeRGB", "mon_a_br": ssdc_scope['brightness'],   "mon_a_cn": ssdc_scope['contrast'],  \
+  "mon_b": "Picture",   "mon_b_br": ssdc_picture['brightness'], "mon_b_cn": ssdc_picture['contrast'] \
+  }
 
   ssdc3 = {}
   ssdc3[ "name" ] = "ssdcfunc"
   ssdc3[ "caption" ] = "Parade YUV | Vectorscope"
-  ssdc3[ "config" ] = { "mon_a": "ParadeYUV", "mon_b": "Vector75" }
+  ssdc3[ "config" ] = { \
+  "mon_a": "ParadeYUV", "mon_a_br": ssdc_scope['brightness'], "mon_a_cn": ssdc_scope['contrast'], \
+  "mon_b": "Vector75",  "mon_b_br": ssdc_scope['brightness'], "mon_b_cn": ssdc_scope['contrast']  \
+  }
 
   ssdc4 = {}
   ssdc4[ "name" ] = "ssdcfunc"
   ssdc4[ "caption" ] = "Parade YUV | Picture"
-  ssdc4[ "config" ] = { "mon_a": "ParadeYUV", "mon_b": "Picture" }
+  ssdc4[ "config" ] = { \
+  "mon_a": "ParadeYUV", "mon_a_br": ssdc_scope['brightness'],   "mon_a_cn": ssdc_scope['contrast'],  \
+  "mon_b": "Picture",   "mon_b_br": ssdc_picture['brightness'], "mon_b_cn": ssdc_picture['contrast'] \
+  }
 
   ssdc5 = {}
   ssdc5[ "name" ] = "ssdcfunc"
-  ssdc5[ "caption" ] = "Audio Dbfs | Histogram"
-  ssdc5[ "config" ] = { "mon_a": "AudioDbfs", "mon_a_xtra": "AudioChannel: 0", "mon_b": "Histogram" }
+  ssdc5[ "caption" ] = "Waveform | Picture"
+  ssdc5[ "config" ] = { \
+  "mon_a": "WaveformLuma", "mon_a_br": ssdc_scope['brightness'], "mon_a_cn": ssdc_scope['contrast'],    \
+  "mon_b": "Picture",      "mon_b_br": ssdc_picture['brightness'], "mon_b_cn": ssdc_picture['contrast'] \
+  }
 
   ssdc6 = {}
   ssdc6[ "name" ] = "ssdcfunc"
-  ssdc6[ "caption" ] = "Picture | Picture"
-  ssdc6[ "config" ] = { "mon_a": "Picture", "mon_b": "Picture" }
+  ssdc6[ "caption" ] = "Audio Dbfs | Histogram"
+  ssdc6[ "config" ] = { \
+  "mon_a": "AudioDbfs", "mon_a_br": ssdc_brightness['scope'], "mon_a_cn": ssdc_contrast['scope'], "mon_a_xtra": "AudioChannel: 0", \
+  "mon_b": "Histogram", "mon_b_br": ssdc_brightness['scope'], "mon_b_cn": ssdc_contrast['scope'] \
+  }
+
+  ssdc7 = {}
+  ssdc7[ "name" ] = "ssdcfunc"
+  ssdc7[ "caption" ] = "Off"
+  ssdc7[ "config" ] = { "mon_a": "Picture", "mon_a_xtra": "Brightness: 0", "mon_b": "Picture", "mon_b_xtra": "Brightness: 0" }
 
   ssdcgrp = {}
   ssdcgrp[ "name" ] = "SmartScope Duo"
-  ssdcgrp[ "actions" ] = ( ssdc1, ssdc2, ssdc3, ssdc4, ssdc5, ssdc6 )
+  ssdcgrp[ "actions" ] = ( ssdc1, ssdc2, ssdc3, ssdc4, ssdc5, ssdc6, ssdc7 )
 
   return( ssdcgrp, )
 
@@ -76,13 +108,12 @@ def customUIAction( info, userData ):
       print( "smartscope-control.py: error connecting to " + ssdc_hostname + ":9992" )
       return
 
-    tcpSocket.setblocking(0)
-    tcpSocket.settimeout(3)
+    tcpSocket.settimeout(1.0)
 
     # Sleep for a little bit to give the SmartScope time to dump its whole
     # preamble.  Yes, this is hacky, but I don't feel like writing a full-blown
     # input read/buffer system just for this
-    time.sleep(0.3)
+    time.sleep(0.2)
 
     # Receive all the preamble info the SmartScope sends us and throw it away
     # because we don't really care.. though maybe at some point we should
@@ -95,17 +126,26 @@ def customUIAction( info, userData ):
 
     # Switch the displays
     if( 'mon_a' in userData['config'] ):
-      tcpSocket.send( "MONITOR A:\n" )
+      tcpSocket.send( "MONITOR A:\n" if ssdc_swap is False else "MONITOR B:\n" )
       tcpSocket.send( "ScopeMode: " + userData['config']['mon_a'] + "\n" )
+      if( 'mon_a_br' in userData['config'] ):
+        tcpSocket.send( "Brightness: " + userData['config']['mon_a_br'] + "\n" )
+      if( 'mon_a_cn' in userData['config'] ):
+        tcpSocket.send( "Contrast: " + userData['config']['mon_a_cn'] + "\n" )
       if( 'mon_a_xtra' in userData['config'] ):
         tcpSocket.send( userData['config']['mon_a_xtra'] + "\n" )
       tcpSocket.send( "\n" )
 
     if( 'mon_b' in userData['config'] ):
-      tcpSocket.send( "MONITOR B:\n" )
+      tcpSocket.send( "MONITOR B:\n" if ssdc_swap is False else "MONITOR A:\n" )
       tcpSocket.send( "ScopeMode: " + userData['config']['mon_b'] + "\n" )
+      if( 'mon_b_br' in userData['config'] ):
+        tcpSocket.send( "Brightness: " + userData['config']['mon_b_br'] + "\n" )
+      if( 'mon_b_cn' in userData['config'] ):
+        tcpSocket.send( "Contrast: " + userData['config']['mon_b_cn'] + "\n" )
       if( 'mon_b_xtra' in userData['config'] ):
         tcpSocket.send( userData['config']['mon_b_xtra'] + "\n" )
       tcpSocket.send( "\n" )
 
+    time.sleep(0.2)
     tcpSocket.close()
